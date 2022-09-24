9d92f1fa22074f45b0bb5ab00442851f4d9bd601
Mekala Chandu <114248527+ChanduMekala-187235@users.noreply.github.com>
2022-09-24 11:09:49 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,26 @@
+package helloworld.java;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=8;
+		int b=7;
+		
+		System.out.println("Before swapping");
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		
+		int temp=a;
+		 a=b;
+		b=temp;
+		
+		System.out.println("after swapping");
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		
+
+	}
+
+
+}
